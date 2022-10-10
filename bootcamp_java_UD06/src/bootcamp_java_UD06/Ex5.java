20105f0c1f285c183bc6d998194f6483aa1ca326
C4rim-cibersegurata <mendezluciano1999@gmail.com>
2022-10-10 23:58:42 +0200

    ejercicio 6

--- bootcamp_java_UD06/src/bootcamp_java_UD06/Ex5.java
+++ bootcamp_java_UD06/src/bootcamp_java_UD06/Ex5.java
@@ -1,28 +1,27 @@
 package bootcamp_java_UD06;
 
+import java.util.Scanner;
+
 public class Ex5 {
 	public static String decToBinary(int num) {
 		String binari = "";
-	     int binary[] = new int[0];    
-	    /* while(num > 0){    
-	       binary[index++] = decimal%2;    
-	       decimal = decimal/2;    
-	     }*/    
-	    /*for(int i = index-1;i >= 0;i--){    
-	       System.out.print(binary[i]);    
-	     }*/ 
-		/*while (num>0) {
+
+		while (num > 0) {
 			if (num % 2 == 0 || num % 2 == 1) {
-				binari = binari + (String.valueOf(num % 2));
+				binari = (String.valueOf(num % 2)) + binari;
 				num = num / 2;
-			} 
-			
-		}*/
+			}
+
+		}
 		return binari;
 	}
 
 	public static void main(String[] args) {
-		System.out.print(decToBinary(31));
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduce el número que quieres pasar a binario: ");
+		int num = sc.nextInt();
+		System.out.print(decToBinary(num));
+		sc.close();
 	}
 
 }
