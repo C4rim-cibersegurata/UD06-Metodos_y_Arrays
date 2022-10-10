b252fc56d90e8be09b07d821a4ddce5464098db3
C4rim-cibersegurata <mendezluciano1999@gmail.com>
2022-10-10 23:49:06 +0200

    ejercicio 6

--- bootcamp_java_UD06/src/bootcamp_java_UD06/Ex5.java
+++ bootcamp_java_UD06/src/bootcamp_java_UD06/Ex5.java
@@ -3,13 +3,21 @@ package bootcamp_java_UD06;
 public class Ex5 {
 	public static String decToBinary(int num) {
 		String binari = "";
-		while (num>0) {
+	     int binary[] = new int[0];    
+	    /* while(num > 0){    
+	       binary[index++] = decimal%2;    
+	       decimal = decimal/2;    
+	     }*/    
+	    /*for(int i = index-1;i >= 0;i--){    
+	       System.out.print(binary[i]);    
+	     }*/ 
+		/*while (num>0) {
 			if (num % 2 == 0 || num % 2 == 1) {
 				binari = binari + (String.valueOf(num % 2));
 				num = num / 2;
 			} 
 			
-		}
+		}*/
 		return binari;
 	}
 
